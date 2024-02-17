e2e0710d4ec190cc05512016d301d1652a63674d
wufake <wufake70@gmail.com>
2024-02-17 16:25:28 +0800

    周六 2024/02/17 16:25:28.02

--- shortcutGit.cmd
+++ shortcutGit.cmd
@@ -3,8 +3,21 @@ chcp 65001 >nul
 color 2
 git add .
 git commit . -m"%date% %time%"
+git push origin master
 set /p input=Go to Git[Y/N]:
-if "%input%"=="y" start https://github.com/wufake70/mySoftwareReverse
-if "%input%"=="Y" start https://github.com/wufake70/mySoftwareReverse
+if "%input%"=="y" goto:GoToGit
+if "%input%"=="Y" goto:GoToGit
+
+
+
+
+
+:GoToGit
+start https://github.com/wufake70/mySoftwareReverse
+
+
+
+
+
 color 7
 pause > nul
