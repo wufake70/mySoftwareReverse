45d1835b0e299eb9b6c8c0e49e8c729fa6e4a9b0
wufake <wufake70@gmail.com>
2024-02-17 16:17:15 +0800

    周六 2024/02/17 16:17:15.62

--- shortcutGit.cmd
+++ shortcutGit.cmd
@@ -5,5 +5,6 @@ git add .
 git commit . -m"%date% %time%"
 set /p input=Go to Git[Y/N]:
 if "%input%"=="y" start https://github.com/wufake70/mySoftwareReverse
+if "%input%"=="Y" start https://github.com/wufake70/mySoftwareReverse
 color 7
 pause > nul
