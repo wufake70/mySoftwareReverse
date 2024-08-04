fe1e19f7ace77e7247f3212fbe4cbb5f0901d1fc
wufake <wufake70@gmail.com>
2024-08-04 22:40:02 +0800

    2024-08-04 22:40:02.34

---
+++ shortcutGit-lfs.cmd
@@ -0,0 +1,20 @@
+@echo off
+chcp 65001 >nul
+color 2
+git add .
+git commit . -m"%date% %time%"
+git push origin master
+set /p input=Go to Git[Y/N]:
+
+if "%input%"=="y" (goto GoToGit)
+if "%input%"=="Y" (goto GoToGit)
+
+color 7
+pause > nul
+exit
+
+:GoToGit
+start https://github.com/wufake70/mySoftwareReverse
+color 7
+pause > nul
+exit
