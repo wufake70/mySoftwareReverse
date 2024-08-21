d33fe8d0a9eef37da9119d58bb4d9a4c10519687
wufake <wufake70@gmail.com>
2024-08-21 22:28:44 +0800

    2024-08-21 22:28:43.91

--- 13_APC/.code/01_ApcInject/Test/Test.cpp
+++ 13_APC/.code/01_ApcInject/Test/Test.cpp
@@ -10,7 +10,8 @@ int main()
 	while (1)
 	{
 		printf("内核达人\r\n");
-		SleepEx(1000, TRUE);
+		SleepEx(1000, TRUE); // 不要使用Sleep函数，该api会将kthread.Alertable置为0，即 不可被唤醒，
+				  // apc无法触发
 	}
 
     return 0;
